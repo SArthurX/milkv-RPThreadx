1617a2ff60a54c450e9e138c5162bd79fac94d1b
SArthurX <sxa05z@gmail.com>
2025-12-18 02:58:33 +0800

    Add mailbox test to control LED

---
+++ mailbox_test/led_rtos.c
@@ -0,0 +1,63 @@
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#define RTOS_CMDQU_DEV_NAME "/dev/cvi-rtos-cmdqu"
+
+typedef struct {
+  unsigned char ip_id;
+  unsigned char cmd_id;
+  unsigned char block;
+  unsigned char resv;
+  unsigned int param_ptr;
+} rtos_cmdqu_t;
+
+#define RTOS_CMDQU_SEND _IOW('r', 1, rtos_cmdqu_t)
+
+#define CMD_DUO_LED 0x13
+
+int main(int argc, char *argv[]) {
+  int fd;
+  int ret;
+  rtos_cmdqu_t cmd;
+
+  if (argc != 2) {
+    printf("Usage: %s <0|1>\n", argv[0]);
+    printf("  2: LED ON\n");
+    printf("  0: LED OFF\n");
+    return -1;
+  }
+
+  int state = atoi(argv[1]);
+
+  fd = open(RTOS_CMDQU_DEV_NAME, O_RDWR);
+  if (fd < 0) {
+    perror("Open device failed");
+    return -1;
+  }
+
+  memset(&cmd, 0, sizeof(cmd));
+  cmd.ip_id = 0;
+  cmd.cmd_id = CMD_DUO_LED;
+  cmd.block = 0;
+  cmd.param_ptr = state;
+
+  printf("Sending CMD: ID=%d, Param=%d to RTOS...\n", cmd.cmd_id,
+         cmd.param_ptr);
+
+  ret = ioctl(fd, RTOS_CMDQU_SEND, &cmd);
+  if (ret < 0) {
+    perror("IOCTL failed");
+    close(fd);
+    return -1;
+  }
+
+  printf("Command sent successfully.\n");
+
+  close(fd);
+  return 0;
+}
